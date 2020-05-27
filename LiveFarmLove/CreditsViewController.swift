fc3db561ee70faa40a932e684c291bec604b0d2b
Alex <alexlehner314@gmail.com>
2020-05-27 11:37:03 -0400

    Made the pages

---
+++ LiveFarmLove/CreditsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreditsViewController.swift
+//  LiveFarmLove
+//
+//  Created by Alexander Lehner on 2020-05-27.
+//  Copyright © 2020 LiveAppLove. All rights reserved.
+//
+
+import UIKit
+
+class CreditsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
